a57a2b3f01f5827dbeecdab1043f8e5639ac60c6
stan lee <stanlee5@naver.com>
2016-05-02 20:05:11 +0900

    Update ccw.cpp

--- Geometry/ccw.cpp
+++ Geometry/ccw.cpp
@@ -1,4 +1,7 @@
-int ccw ()
+typedef pair<int,int> point;
+
+long long ccw(point a, point b, point c)
 {
-  return 0;
+	return (a.first*b.second + b.first*c.second + c.first*a.second)
+		- (a.first*c.second + b.first*a.second + c.first*b.second);
 }
