15249eb865cc2811d4cfa4320de1adbd5247805a
stan lee <stanlee5@naver.com>
2016-05-02 20:09:21 +0900

    Update convexhull.cpp

--- Geometry/convexhull.cpp
+++ Geometry/convexhull.cpp
@@ -7,17 +7,17 @@ using namespace std;
 typedef pair<int,int> point;
 typedef vector<point> points;
 
-long long ccw(point a, point b, point c)
+long long ccw (point a, point b, point c)
 {
 	return (a.first*b.second + b.first*c.second + c.first*a.second)
 		- (a.first*c.second + b.first*a.second + c.first*b.second);
 }
 
-points convexHull(points a)
+points convexHull (points a)
 {
 	points up,dw;
-	sort(a.begin(), a.end());
-	for(int i=0;i<a.size();i++)
+	sort (a.begin(), a.end());
+	for (int i=0;i<a.size();i++)
 	{
 		while (up.size()>=2 && ccw(*++up.rbegin(),*up.rbegin(),a[i])>=0) up.pop_back();
 		while (dw.size()>=2 && ccw(*++dw.rbegin(),*dw.rbegin(),a[i])<=0) dw.pop_back();
